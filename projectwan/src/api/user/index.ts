78493ce2146f644d7854e366cadd11001ec39d3a
Eterna-WanZi <106149406+Eterna-WanZi@users.noreply.github.com>
2025-06-27 15:33:09 +0800

    Delete projectwan directory

--- projectwan/src/api/user/index.ts
+++
@@ -1,9 +0,0 @@
-import { POST } from '@/untils/request';
-import type { loginFrom } from './type';
-export const login = (data: loginFrom) =>
-  POST({
-    url: '/login',
-    params: {
-      data: data
-    }
-  });
